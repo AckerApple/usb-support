724ae2649100e0d9381ceefccc39b9c0551b3c80
Acker Apple <y>
2021-04-05 22:02:53 -0400

    update

--- webapp/src/app/app.component.ts
+++ webapp/src/app/app.component.ts
@@ -5,6 +5,7 @@ import mapController from './mapController.function'
 import { socketHost, socketPort } from '../../../shared/config.json'
 import { devicesMatch, isDeviceController } from '../../../index.shared'
 import decodeDeviceMetaState from './decodeControllerButtonStates.function'
+import { ack } from 'ack-x/js/ack'
 
 export interface IDeviceMetaState extends IDeviceMeta {
   subscribed: boolean
@@ -19,46 +20,62 @@ export interface IDeviceMetaState extends IDeviceMeta {
 })
 export class AppComponent {
   title = 'webapp'
-  url = `ws://${socketHost}:${socketPort}`
-  connection = new WebSocket(this.url)
+  wsUrl = `ws://${socketHost}:${socketPort}`
+  connection: WebSocket
 
   devices: IDeviceMetaState[] = []
   listeners: IDeviceMetaState[] = []
   controllers: IDeviceMeta[] = []
   nonControllers: IDeviceMeta[] = []
-  savedControllers: Record<string, any>
+  savedControllers: Record<string, any> = {}
   savedController: Record<string, any>
 
   debug = {
     state: 'initializing',
     messages: 0,
-    url: this.url,
+    url: this.wsUrl,
     socket: {},
     lastSubscription: {},
     lastPayload: {},
+    lastlLogs: {
+      info:{}, error: {}
+    }
   }
 
   constructor() {
-    console.log('initializing')
+    this.log('initializing')
 
     this.debug.state = 'constructing'
-    window.onerror = function (msg, url, line) {
-      this.debug.windowError = {msg, url, line}
+    window.onerror = err => this.error(typeof(err) === 'string' ? new Error(err) : err)
+
+    this.log('making socket connection handshake')
+
+    try {
+      this.connect()
+      this.debug.state = 'constructed'
+      this.log('web socket connected')
+    } catch (err) {
+      this.error(err, {
+        message: 'Could not connect to web sockets',
+      })
     }
+  }
 
-    console.log('making socket connection handshake')
-
+  connect() {
+    this.connection = new WebSocket(this.wsUrl)
     this.connection.onopen = () => {
       // connection.send('Message From Client')
-      console.log('web socket handshake successful')
+      this.log('web socket handshake successful')
       this.fetchUsbDevices()
       this.fetchSavedControllers()
       this.debug.state = 'socket opened'
     }
 
     this.connection.onerror = (ev: Event): any => {
-      (this.debug.socket as any).error = JSON.stringify(ev, Object.getOwnPropertyNames(ev));
-      console.log(`Socket error`, (ev as any).message, JSON.stringify(ev, null, 2))
+      this.error(
+        ev,
+        {message: `Socket error`},
+      )
     }
 
     this.connection.onmessage = (e) => {
@@ -70,32 +87,34 @@ export class AppComponent {
         const data = JSON.parse(e.data)
         this.handleMessage(data)
       } catch (err) {
-        console.error('client message failed', err);
+        this.error(err, 'client message failed');
         (this.debug.socket as any).error = err
       }
     }
-    this.debug.state = 'constructed'
   }
 
   mapController(controller: GameControlEvents) {
     mapController(controller)
   }
 
-  handleMessage(data) {
+  handleMessage(data: SocketMessage) {
     switch (data.type) {
       case 'devices':
         this.devices = data.devices
         this.controllers = this.devices.filter(device => isDeviceController(device))
         this.nonControllers = this.devices.filter(device => !isDeviceController(device))
-        console.log('devices', data.devices.length, this.devices.length)
+        this.log('devices', data.devices.length, this.devices.length)
         break;
 
       case 'savedControllers':
         this.savedControllers = data.controllers
-        console.log('savedControllers', data.controllers)
+        this.log('savedControllers', data.controllers)
         break;
 
       case 'listeners':
+        this.log({
+          message: `new listeners received ${this.listeners.length}`
+        })
         this.listeners = data.devices
         this.listeners.forEach(lDevice => {
           this.devices.forEach(device => {
@@ -107,6 +126,10 @@ export class AppComponent {
         break
 
       case 'deviceEvent.change':
+        this.log({
+          message: `device change event`
+        })
+
         const matchedListener = this.listeners.find(device => devicesMatch(device, data.device))
 
         if (matchedListener) {
@@ -121,8 +144,17 @@ export class AppComponent {
         break
 
       default:
-        console.warn('unknown event type', data.type)
+        this.warn('unknown event type', data.type)
     }
+  }
+
+  addTestController() {
+    this.controllers.push(testController)
+    this.devices.push({
+      subscribed: false,
+      lastEvent: {},
+      ...testController
+    })
   }
 
   fetchSavedControllers() {
@@ -150,6 +182,12 @@ export class AppComponent {
   }
 
   listenToDevice(device: IDeviceMetaState) {
+    const stringRef = `${device.product} by ${device.manufacturer}`
+
+    this.log({
+      message: `attempting to listen to ${stringRef}`
+    })
+
     const payload = {
       type: 'listenToDevice', device
     }
@@ -159,6 +197,9 @@ export class AppComponent {
       payload.type = 'unsubscribeToDevice'
       delete device.subscribed
       delete deviceMatch.subscribed
+      this.log({
+        message: `Unsubscribed from ${stringRef}`
+      })
     }
 
     const savedControllers =  Object.values(this.savedControllers).reduce((all, current) => [...all,...Object.values(current)], [])
@@ -169,6 +210,58 @@ export class AppComponent {
     }
 
     this.debug.lastPayload = payload
+    this.log({
+      message: `requesting web socket to listen to ${stringRef}`
+    })
     this.connection.send(JSON.stringify(payload))
+
+    if (device === testController) {
+      this.handleMessage({
+        type: 'listeners',
+        devices: this.devices,
+        controllers: this.controllers,
+        event: {message:'test-event'},
+        device
+      })
+    }
   }
+
+  error(error: Event | Error, ...extra) {
+    if (typeof error === 'string') {
+      error = new Error(error)
+    }
+    const readable = ack.error(error).toObject()
+    this.debug.lastlLogs.error = {...readable, ...error, ...extra.reduce((all, one) => ({...all, ...one}), {})}
+    console.error(error, extra)
+  }
+
+  warn(...data: any) {
+    this.debug.lastlLogs.info = data
+    console.warn(data)
+  }
+
+  log(...data: any) {
+    this.debug.lastlLogs.info = data
+    console.log(data)
+  }
+}
+
+const testController: IDeviceMeta = {
+  path: 'test-path',
+  interface: -1,
+  usage: -1,
+  usagePage: -1,
+  productId: -1,
+  vendorId: -32,
+  product: 'test-product',
+  manufacturer: 'test-manu',
+
+}
+
+interface SocketMessage {
+  type: 'deviceEvent.change' | 'listeners' | 'savedControllers' | 'devices'
+  devices: IDeviceMetaState[]
+  controllers: IDeviceMeta[]
+  event?: any // Event
+  device?: IDeviceMeta
 }
